f43a9579794efec0556db9dcbf0d7b9dd10409b0
Daniel Fernandes <danielferdsc@gmail.com>
2025-07-13 23:49:54 -0300

    chore: configuração incial do backend.

---
+++ backend/src/test/java/com/bookworms/backend/BookwormsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookworms.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookwormsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
