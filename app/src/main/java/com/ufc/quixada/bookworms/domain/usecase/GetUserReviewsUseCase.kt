e370118493246eb972c4daef03c9ebcb47f27164
Tiago Tito <tiago.tito@alu.ufc.br>
2026-01-10 11:14:03 -0300

    terminei casos de uso review

--- app/src/main/java/com/ufc/quixada/bookworms/domain/usecase/GetUserReviewsUseCase.kt
+++ app/src/main/java/com/ufc/quixada/bookworms/domain/usecase/GetUserReviewsUseCase.kt
@@ -1,4 +1,24 @@
 package com.ufc.quixada.bookworms.domain.usecase
 
-class GetUserReviewsUseCase {
+import com.ufc.quixada.bookworms.domain.repository.ReviewRepository
+import com.ufc.quixada.bookworms.domain.repository.ReviewResult
+import com.ufc.quixada.bookworms.domain.repository.UserRepository
+import com.ufc.quixada.bookworms.domain.repository.UserResult
+import javax.inject.Inject
+
+class GetUserReviewsUseCase @Inject constructor(
+    private val reviewRepository: ReviewRepository,
+    private val userRepository: UserRepository
+) {
+    suspend operator fun invoke(
+        userId: String
+    ): ReviewResult {
+        val userResult = userRepository.getUser(userId)
+
+        if (userResult is UserResult.Error) {
+            return ReviewResult.Error("Erro ao buscar usuário: ${userResult.message}")
+        }
+
+        return reviewRepository.getReviewsByUser(userId)
+    }
 }
--- app/src/main/java/com/ufc/quixada/bookworms/domain/usecase/UpdateReviewUseCase.kt
+++ app/src/main/java/com/ufc/quixada/bookworms/domain/usecase/UpdateReviewUseCase.kt
@@ -1,4 +1,46 @@
 package com.ufc.quixada.bookworms.domain.usecase
 
-class UpdateReviewUseCase {
+import com.ufc.quixada.bookworms.domain.model.Review
+import com.ufc.quixada.bookworms.domain.repository.ReviewRepository
+import com.ufc.quixada.bookworms.domain.repository.ReviewResult
+import com.ufc.quixada.bookworms.domain.repository.SingleReviewResult
+import java.util.Date
+import java.util.UUID
+import javax.inject.Inject
+
+class UpdateReviewUseCase @Inject constructor(
+    private val reviewRepository: ReviewRepository
+) {
+    suspend operator fun invoke(
+        reviewId: String,
+        nota: Int,
+        textoResenha: String,
+        contemSpoiler: Boolean
+    ): SingleReviewResult {
+        //verifica se a review existe e guarda as informações antigas
+        val oldReview = when(val reviewResult = reviewRepository.getReviewById(reviewId)) {
+            is SingleReviewResult.Error -> {
+                return SingleReviewResult.Error("Erro ao buscar review: ${reviewResult.message}")
+            }
+            is SingleReviewResult.Success -> {
+                reviewResult.data
+            }
+        }
+
+        if (nota !in 1..5){
+            return SingleReviewResult.Error("A nota deve estar entre 1 e 5")
+        }
+
+        val updatedReview = Review(
+            reviewId = oldReview.reviewId,
+            userId = oldReview.userId,
+            bookId = oldReview.bookId,
+            nota = nota,
+            textoResenha = textoResenha,
+            contemSpoiler = contemSpoiler,
+            dataAtualizacao = Date()
+        )
+
+        return reviewRepository.updateReview(updatedReview)
+    }
 }
