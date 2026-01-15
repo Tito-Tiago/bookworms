c6c5cb3eb096725a2c480b94df9f6428dd726d02
Tiago Tito <tiago.tito@alu.ufc.br>
2026-01-14 23:08:32 -0300

    ajeitei formatação notaMediaDaComunidade

--- app/src/main/java/com/ufc/quixada/bookworms/presentation/book_details/BookDetailsScreen.kt
+++ app/src/main/java/com/ufc/quixada/bookworms/presentation/book_details/BookDetailsScreen.kt
@@ -231,7 +231,7 @@ fun BookDetailsScreen(
                             ) {
                                 Icon(Icons.Default.Star, null, tint = Color(0xFFFFD700), modifier = Modifier.size(18.dp))
                                 Spacer(modifier = Modifier.width(4.dp))
-                                Text("${book.notaMediaComunidade}", fontWeight = FontWeight.Bold)
+                                Text("%.1f".format(book.notaMediaComunidade), fontWeight = FontWeight.Bold)
                             }
 
                             Button(
